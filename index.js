b494870542fa5b5ef15f1beae64e33449e6283b8
Ruth Moraa <moraar37@gmail.com>
2022-12-23 22:39:06 +0300

    adding html, css and variables to manipulate the DOM

---
+++ index.js
@@ -0,0 +1,33 @@
+// let weather = {
+//     "apiKey": "1f69fc603ff2d4b4c44901ea991ea5af",
+//     fetchWeather: function (city) {
+//         fetch(
+//           "https://api.openweathermap.org/data/2.5/weather?q=" +
+//             city +
+//             "&units=metric&appid=" +
+//             this.apiKey
+//         )
+//           .then((response) => {
+//             if (!response.ok) {
+//               alert("No weather found.");
+//               throw new Error("No weather found.");
+//             }
+//             return response.json();
+//           })
+//           .then((data) => console.log(data));
+//       },
+//       displayWeather: function (data){
+
+//       }
+//     }
+
+// adding variables that will help in DOM
+const button = document.querySelector('#button')
+const form = document.querySelector("#form"),
+inputPart = form.querySelector("#city")
+const country = document.querySelector('#country')
+const icon = document.querySelector('#icon')
+const description = document.querySelector('#description')
+const temp = document.querySelector('#temp')
+
+
