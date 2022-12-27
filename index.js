56d53705a1de2b344d65047602df367aa64ec624
Ruth Moraa <moraar37@gmail.com>
2022-12-27 07:29:57 +0300

    iporting goggle fonts

--- index.js
+++ index.js
@@ -1,16 +1,68 @@
-let weather = async () => {
+document.querySelector('#city').addEventListener('click',() => 
+    console.log(form.childNodes[1])
+)
 
-fetch('https://api.openweathermap.org/data/2.5/weather?q=nairobi&appid=220789f4e8c25f055bb2d1eae7f527ab')
+
+fetch('https://api.openweathermap.org/data/2.5/weather?q=city,{country code}&appid=220789f4e8c25f055bb2d1eae7f527ab')
 .then(res => res.json())
-.then(data => console.log(data))
+.then(data => console.log(data)
 
-}
-console.log (weather())
-document.querySelector('#butt').addEventListener('click',  ()  => {
-    console.log (weather())
+    // {
+    //  console.log(data['name'])
+        // var nameValue = console.log(data['name']) 
+        // var tempValue = console.log(data['main']['temp'])
+        //   var descriptionValue = console.log(data['weather'][0]['description'])
+         
+         
+          
+        // country.innerHTML = console.log(nameValue);
+                    
+        // description.innerHTML = descriptionValue;
+        //  temp.innerHTML = tempValue;
 
-})
 
+
+// }
+);
+
+
+   
+   
+//below works
+
+
+// let weather = async () => {
+
+
+// fetch('https://api.openweathermap.org/data/2.5/weather?q=nairobi&appid=220789f4e8c25f055bb2d1eae7f527ab')
+// .then(res => res.json())
+// .then(data => console.log(data)
+    // {
+//     var nameValue = data['name'];
+//    var tempValue = data['main']['temp'];
+//      var descriptionValue = data['weather'][0]['description'];
+    
+    
+              
+//    country.innerHTML = nameValue;
+               
+//    description.innerHTML = descriptionValue;
+//     temp.innerHTML = tempValue;
+    
+//   })
+// )
+
+// }
+
+
+
+// document.querySelector('#butt').addEventListener('click',  ()  => {
+  
+
+// })
+
+
+//above works
 
 
 // )
