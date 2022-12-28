bc0893371e02aae7103ab3ee1ce01308e37526d9
Ruth Moraa <moraar37@gmail.com>
2022-12-28 14:34:30 +0300

    editting to ensure proper display of icons

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const card = wrapper.querySelector('.card text-center')
 const city = wrapper.querySelector('#city')
 const getLocation = wrapper.querySelector('#butt')
 let api;
+let wIcon = document.querySelector("#icon")
 
 
 city.addEventListener('keyup', e => {
@@ -57,13 +58,40 @@ function weatherDetails(info) {
         const row = document.querySelector('.row')
         const cards = row.querySelector('#cards')
         const country = cards.querySelector('#country')
-              
+         const id = cards.querySelector('#icon')
         const temp = cards.querySelector('#temp')
         const description = cards.querySelector('#description')
         
 
         //using DOM to display weather details
         country.innerText = (info['name']);
+        id.innerText= console.log(  (info['weather'][0]['id']))
+
+        //using custom icon according to the weather data received from api
+        if((info['weather'][0]['id']) == 800){
+            wIcon.src = "./icons/Weather Icons/clear.svg"
+
+        }
+        else if((info['weather'][0]['id'])  >= 200 && (info['weather'][0]['id']) <= 232){
+            wIcon.src = "./icons/Weather Icons/storm.svg"
+
+        }
+        else if((info['weather'][0]['id'])  >= 600 && (info['weather'][0]['id']) <= 622){
+            wIcon.src = "./icons/Weather Icons/snow.svg"
+
+        }
+        else if((info['weather'][0]['id'])  >= 701 && (info['weather'][0]['id']) <= 781){
+            wIcon.src = "./icons/Weather Icons/haze.svg"
+
+        }
+        else if((info['weather'][0]['id']) >= 801 && (info['weather'][0]['id']) <= 804){
+            wIcon.src = "./icons/Weather Icons/cloud.svg"
+
+        }
+        else if(((info['weather'][0]['id']) >= 300 && (info['weather'][0]['id'])<= 321) || ((info['weather'][0]['id']) >= 500 && (info['weather'][0]['id']) <= 531)){
+            wIcon.src = "./icons/Weather Icons/rain.svg"
+
+        }
         //using math floor to set temperature to whole numbers without decimal places
         temp.innerHTML = Math.floor((info['main']['temp']));
         description.innerHTML = (info['weather'][0]['description'])
